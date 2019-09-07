3c07e4f4f7d9e5a2f12821055e77af443ccd5093
Daniela Alvarado Pereda <danielaalvaradopereda@hotmail.com>
2019-09-07 13:32:04 -0500

    fixed env variable issue

--- src/db/migrations/1567629499353_note.js
+++ src/db/migrations/1567629499353_note.js
@@ -2,7 +2,7 @@ exports.up = (knex) => {
     return knex.schema.createTable('notes', (table) => {
         table.increments('id').primary();
         table.integer('user_id').unsigned().notNullable();
-        table.string('content').notNullable();
+        table.text('content').notNullable();
         table.string('title');
         table.timestamps(true, true);
     });
